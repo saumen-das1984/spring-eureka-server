a61e43f68e5bfb27cb4e821c626673ddc366fb76
E0594047 <SaumenDas@eaton.com>
2020-10-29 12:00:48 +0530

    Initial Commit

---
+++ src/test/java/com/signify/test/logging/level/eureka/server/springeurekaserver/SpringEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signify.test.logging.level.eureka.server.springeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
